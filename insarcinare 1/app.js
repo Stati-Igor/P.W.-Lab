a95c4b9d0933e9b56f261cfecebc4c102923827f
Stati-Igor <statiigor1@gmail.com>
2021-01-19 19:45:22 +0200

    switc case

---
+++ insarcinare 1/app.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOM ContentLoaded"), () =>{
+let digit=prompt();
+switch(digit){
+    case 0:
+        console.log("zero");
+        break;
+    case 1:
+        console.log("zero");
+        break;      
+    case 2:
+        console.log("zero");
+        break;
+    case 3:
+        console.log("zero");
+        break; 
+    case 4:
+        console.log("zero");
+        break;
+    case 5:
+        console.log("zero");
+        break; 
+    case 6:
+        console.log("zero");
+        break;
+    case 7:
+        console.log("zero");
+        break; 
+    case 8:
+        console.log("zero");
+        break;
+    case 9:
+        console.log("zero");
+        break;  
+    default:
+        console.log("Nu este cifra");  
+}
+}
