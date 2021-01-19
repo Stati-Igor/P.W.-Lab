7cec4ad5fc047d93555a6dfb1d0a6cb8f9990e97
Stati-Igor <statiigor1@gmail.com>
2021-01-19 20:06:53 +0200

    corectare

--- insarcinare 1/app.js
+++ insarcinare 1/app.js
@@ -1,37 +1,39 @@
-document.addEventListener("DOM ContentLoaded"), () =>{
+document.addEventListener('DOMContentLoaded', () =>{
 let digit=prompt();
 switch(digit){
-    case 0:
+    case '0':
         console.log("zero");
         break;
-    case 1:
-        console.log("zero");
+    case '1':
+        console.log("unu");
         break;      
-    case 2:
-        console.log("zero");
+    case '2':
+        console.log("doi");
         break;
-    case 3:
-        console.log("zero");
+    case '3':
+        console.log("trei");
         break; 
-    case 4:
-        console.log("zero");
+    case '4':
+        console.log("patru");
         break;
-    case 5:
-        console.log("zero");
+    case '5':
+        console.log("cinci");
         break; 
-    case 6:
-        console.log("zero");
+    case '6':
+        console.log("sase");
         break;
-    case 7:
-        console.log("zero");
+    case '7':
+        console.log("sapte");
         break; 
-    case 8:
-        console.log("zero");
+    case '8':
+        console.log("8");
         break;
-    case 9:
-        console.log("zero");
+    case '9':
+        console.log("9");
         break;  
     default:
         console.log("Nu este cifra");  
-}
-}
+    }
+    });
+
+   
