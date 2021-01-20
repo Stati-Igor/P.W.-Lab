70644fb052a74b6f50314d9952a0c2ef18c32561
Stati-Igor <statiigor1@gmail.com>
2021-01-20 17:38:59 +0200

    insarcinarea 2

---
+++ InsarcinareNr2/app.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    var txt;
+    var numar = prompt("Intoduceti un numar");
+    if (numar > 0) {
+      alert("1");
+    } else if (numar < 0) {
+      alert("-1");
+    } else if (numar == 0) {
+      alert("0");
+    } else{
+      alert("Error");
+    }
+  }
