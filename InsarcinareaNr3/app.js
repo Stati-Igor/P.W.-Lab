d2d3e08ee158859aa7335b87a8ca8a6771c5bd3d
Stati-Igor <statiigor1@gmail.com>
2021-01-20 18:25:31 +0200

    v1

---
+++ InsarcinareaNr3/app.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    var muzica = ["Jazz", "Blues"];
+    alert(muzica)
+    var ciclu;
+for(ciclu = 0; ciclu < 5; ciclu++)
+switch(ciclu){
+    case 0: muzica.push("Rock-and-roll"); alert(muzica);
+        break;
+    case 1: muzica.push("Rock-and-roll"); alert(muzica);
+        break;
+    case 2: muzica.push("Rock-and-roll"); alert(muzica);
+        break;
+    case 3: muzica.push("Rock-and-roll"); alert(muzica);
+        break;
+    case 4: muzica.push("Rock-and-roll"); alert(muzica);
+        break;
+}
+});
