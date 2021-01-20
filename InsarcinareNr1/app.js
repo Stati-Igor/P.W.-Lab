ffbf023eaaf151f63010bfca2b936ae78a221e05
Stati-Igor <statiigor1@gmail.com>
2021-01-20 15:33:13 +0200

    corectare

--- InsarcinareNr1/app.js
+++ InsarcinareNr1/app.js
@@ -1,19 +1,23 @@
-function sum (obj)
-{var n=obj.num1.value;
- var n1=obj.num2.value;
-var s=0
-if(n<n1 && n>=0){
-for(var i = n; i<=n1; i++){
-   if(i>0 && i%2==0){
- s+=i;
-  }
-    obj.res.value=s;
-
+function sumPar (){
+var n1=parseInt(document.getElementById("n1").value);
+var n2=parseInt(document.getElementById("n2").value);
+var s=0;
+var i;
+if(n1<n2){
+for(var i = n1; i<=n2; i++){
+   if(i%2==0){
+   s+=i;
+   }
 }
+alert("Suma numerelor pare din intervalul ales este: "+s);
+}else if(n1>n2){
+    for(var i = n2; i<=n1; i++){
+       if(i%2==0){
+      s+=i;
+       }
+    }
+alert("Suma numerelor pare din intervalul ales este: "+s);
+}else if(n1==n2){
+    alert("Alegeti un interval corect");
 }
-else{
-  obj.res.value='Eroare de introducere';
-}
-n=null;
-n1=null;
 }
