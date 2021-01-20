202a3c744c4af3ff90be583b14530f82a3bfc6bf
Stati-Igor <statiigor1@gmail.com>
2021-01-20 22:42:06 +0200

    v final

--- InsarcinareaNr3/app.js
+++ InsarcinareaNr3/app.js
@@ -2,17 +2,28 @@ document.addEventListener('DOMContentLoaded', () => {
     var muzica = ["Jazz", "Blues"];
     alert(muzica)
     var ciclu;
-for(ciclu = 0; ciclu < 5; ciclu++)
+    var temp;
+for(ciclu = 0; ciclu < 5; ciclu++){
 switch(ciclu){
-    case 0: muzica.push("Rock-and-roll"); alert(muzica);
+    case 0: 
+        muzica.push("Rock-and-roll"); 
+        alert(muzica);
         break;
-    case 1: muzica.push("Rock-and-roll"); alert(muzica);
+    case 1: 
+        muzica[muzica.length / 2 | 0] = "Clasica"; 
+        alert(muzica);
         break;
-    case 2: muzica.push("Rock-and-roll"); alert(muzica);
+    case 2: 
+        temp = muzica[0]
+        muzica.splice(0, 1);
+        alert("Acest element: "+temp+" (primul)- a fost sters. Iata matricea obtinuta "+muzica);
         break;
-    case 3: muzica.push("Rock-and-roll"); alert(muzica);
+    case 3: 
+        muzica.splice(0, 0, "Rap", "Reggae"); 
+        alert(muzica);
         break;
-    case 4: muzica.push("Rock-and-roll"); alert(muzica);
+    default: 
         break;
+}
 }
 });
