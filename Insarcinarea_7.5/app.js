7deaf88abc5114d8d353b5d79d11e1a9e7a347b7
Stati-Igor <statiigor1@gmail.com>
2021-05-15 22:16:54 +0300

    Insarcinarea7.5

---
+++ Insarcinarea_7.5/app.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    document.getElementById("input").disabled = true;    
+})
+var num =parseInt(document.getElementById("input").value);
+document.getElementById("-").addEventListener("click", ()=>{ 
+    var num =parseInt(document.getElementById("input").value);
+    if(num>1 & num<10){
+        document.getElementById("input").stepDown(1);
+        document.getElementById("+").disabled = false;
+    }else if(num == 1){
+        document.getElementById("input").stepDown(1);
+        document.getElementById("-").disabled = true;
+    }
+})
+document.getElementById("+").addEventListener("click", ()=>{ 
+    var num =parseInt(document.getElementById("input").value);
+    if(num>-1 & num<8){
+        document.getElementById("input").stepUp(1);
+        document.getElementById("-").disabled = false;
+    }else if(num == 8){
+        document.getElementById("input").stepUp(1);
+        document.getElementById("+").disabled = true;
+    }
+})
+
