0c59f1151a66f62308e4ef05cbe9df4689786257
Stati-Igor <statiigor1@gmail.com>
2021-05-16 12:42:22 +0300

    tabla sah

---
+++ Insarcinarea_7.6/app.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    document.getElementById("tabla").style.borderCollapse = "collapse";
+        var x = document.querySelectorAll(".sah");
+        var i;
+        for (i = 0; i < x.length; i++) {
+          x[i].style.backgroundColor = "black";
+        }
+})
+
+
