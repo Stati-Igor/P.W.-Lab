39e7103defcd5ffd73af2f39a784696dac7bda4c
Stati-Igor <statiigor1@gmail.com>
2021-11-09 16:35:38 +0200

    insarcinare afisare

---
+++ WEB2-insarcinare1/Main.java
@@ -0,0 +1,5 @@
+public class public static void main(String[] args) {
+    Message newMessage=new afisare();
+    newMessage.setMessage("Hello world !!");
+    System.out.println(newMessage.getMessage())
+}
